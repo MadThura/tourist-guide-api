e71a137ff21372413056af0e0eb04be9793f22a3
+ <+>
2025-06-20 00:52:56 +0630

    user register api

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    public function store()
+    {
+        $validator = Validator::make(request()->all(), [
+            'name' => ['required', 'min:2', 'max:50'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:6', 'max:30']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => request('password')
+        ]);
+
+        $token = $user->createToken('user-token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::post('/users/register', [UserController::class, 'store']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
