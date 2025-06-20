52f970dc7be50cb1694397d0186aa06e75eea2ef
+ <+>
2025-06-20 09:40:03 +0630

    create user login api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
@@ -38,5 +39,38 @@ class UserController extends Controller
             'user' => $user,
             'token' => $token,
         ], 201);
+    }
+
+
+    public function login()
+    {
+        $validator = validator(request()->all(), [
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:6']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::where('email', request('email'))->first();
+
+        if (!$user || !Hash::check(request('password'), $user->password)) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Invalid credentials.'
+            ], 401);
+        }
+
+        $token = $user->createToken('user-token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'token' => $token
+        ],);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/users/register', [UserController::class, 'store']);
+Route::post('/users/login', [UserController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
